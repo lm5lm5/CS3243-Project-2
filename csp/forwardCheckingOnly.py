41c46f511ed256a72532b519803024b7c55d182b
unknown <ooimingsheng@gmail.com>
2020-05-06 22:42:19 +0800

    Add forwarc checking only

---
+++ csp/forwardCheckingOnly.py
@@ -0,0 +1,217 @@
+import sys
+import copy
+import time
+from heapq import heappop, heappush, heapify
+
+# Running script: given code can be run with the command:
+# python file.py, ./path/to/init_state.txt ./output/output.txt
+
+import collections
+
+class OrderedSet(collections.MutableSet):
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[1]
+            curr[2] = end[1] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[2] = next
+            next[1] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[2]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[2]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[1]
+        while curr is not end:
+            yield curr[0]
+            curr = curr[1]
+
+    def pop(self, last=True):
+        if not self:
+            return None
+        key = self.end[1][0] if last else self.end[2][0]
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
+
+class Sudoku(object):
+    def __init__(self, puzzle):
+        # you may add more attributes if you need
+        self.puzzle = puzzle # self.puzzle is a list of lists
+        self.ans = copy.deepcopy(puzzle) # self.ans is a list of lists
+
+    def solve(self):
+        nine = 9
+        visitedRows = [[False for j in range(10)] for i in range(9)]
+        visitedCols = [[False for j in range(10)] for i in range(9)]
+        visitedBlocks = [[[False for k in range(10)] for i in range(3)] for j in range(3)]
+        isSafe = lambda row, col, num: (not visitedRows[row][num]) and (not visitedCols[col][num])  and (not visitedBlocks[row // 3][col // 3][num])
+        
+        for i in range(9):
+            for j in range(9):
+                num = puzzle[i][j]
+                if num != 0:
+                    visitedRows[i][num] = True
+                    visitedCols[j][num] = True
+
+                    x = i // 3
+                    y = j // 3
+                    visitedBlocks[x][y][num] = True
+
+
+        possibleValuesList = [[list() for j in range(9)] for i in range(9)]
+        for i in range(9):
+            for j in range(9):
+                for k in range(1, 10):
+                    if isSafe(i, j, k):
+                        possibleValuesList[i][j].append(k)
+
+        setCells = []                           # stack of set cells
+        unsetCells = OrderedSet()
+        hasUnfilledCells = False
+
+        for i in range(9):
+            for j in range(9):
+                if puzzle[i][j] == 0:
+                    unsetCells.add(i * nine + j)
+                    hasUnfilledCells = True
+
+        if not hasUnfilledCells:
+            return self.ans
+
+        
+
+        def getNextUnsetCell():
+            return unsetCells.pop()
+
+        currElement = getNextUnsetCell()
+        currR = currElement // nine
+        currC = currElement % nine
+        index = 0
+
+        backtracks = 0
+        # Main backtracking loop
+        while True:
+            length = len(possibleValuesList[currR][currC])
+            while index < length:
+                currVal = possibleValuesList[currR][currC][index]
+                if isSafe(currR, currC, currVal):
+                    break
+                else:
+                    index += 1
+
+            if index < length:
+                self.ans[currR][currC] = currVal
+                visitedRows[currR][currVal] = True
+                visitedCols[currC][currVal] = True
+                
+                blockR = currR // 3
+                blockC = currC // 3
+                visitedBlocks[blockR][blockC][currVal] = True
+
+                setCells.append(currR * 100 + currC * 10 + index)
+
+                nextUnsetCell = getNextUnsetCell()
+
+                if nextUnsetCell == None:
+                    print("backtracks ", backtracks)
+                    return self.ans
+
+                currR = nextUnsetCell // nine
+                currC = nextUnsetCell % nine
+                index = 0 
+
+            elif len(setCells) > 0: # Backtrack if no more available numbers
+                unsetCells.add(currR * nine + currC)
+                backtracks += 1
+                self.ans[currR][currC] = 0 
+                x = setCells.pop()
+                currR = (x // 100) % 10
+                currC = (x // 10) % 10
+                index = x % 10
+                currVal = possibleValuesList[currR][currC][index]
+                self.ans[currR][currC] = 0 
+                visitedRows[currR][currVal] = False
+                visitedCols[currC][currVal] = False
+                blockR = currR // 3
+                blockC = currC // 3
+                visitedBlocks[blockR][blockC][currVal] = False
+                index += 1
+
+        return None
+
+    # you may add more classes/functions if you think is useful
+    # However, ensure all the classes/functions are in this file ONLY
+    # Note that our evaluation scripts only call the solve method.
+    # Any other methods that you write should be used within the solve() method.
+
+if __name__ == "__main__":
+    # STRICTLY do NOT modify the code in the main function here
+    if len(sys.argv) != 3:
+        print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
+        raise ValueError("Wrong number of arguments!")
+
+    try:
+        f = open(sys.argv[1], 'r')
+    except IOError:
+        print ("\nUsage: python CS3243_P2_Sudoku_XX.py input.txt output.txt\n")
+        raise IOError("Input file not found!")
+
+    start_time = time.time()
+
+    puzzle = [[0 for i in range(9)] for j in range(9)]
+    lines = f.readlines()
+
+    i, j = 0, 0
+    for line in lines:
+        for number in line:
+            if '0' <= number <= '9':
+                puzzle[i][j] = int(number)
+                j += 1
+                if j == 9:
+                    i += 1
+                    j = 0
+
+    sudoku = Sudoku(puzzle)
+    ans = sudoku.solve()
+
+    print("time elapsed in seconds: %s" % (time.time() - start_time))
+
+    with open(sys.argv[2], 'a') as f:
+        for i in range(9):
+            for j in range(9):
+                f.write(str(ans[i][j]) + " ")
+            f.write("\n")
